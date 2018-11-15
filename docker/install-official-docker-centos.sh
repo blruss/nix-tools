ab5b18d20bc60debeaf4a39391d488cdd83f944b
Blake Russo <43853278+blruss@users.noreply.github.com>
2018-11-15 14:36:46 +0000

    Create install-official-docker-centos.sh

---
+++ docker/install-official-docker-centos.sh
@@ -0,0 +1,14 @@
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum-config-manager --enable docker-ce-edge
+sudo yum-config-manager --enable docker-ce-test
+#sudo yum-config-manager --disable docker-ce-edge  
+sudo yum install docker-ce
+sudo systemctl status docker
+sudo systemctl start docker
+ 
+ echo "Don't forget to create /etc/docker/daemon.json"
+ sudo systemctl stop docker
+#test docker
+#sudo docker run hello-world
